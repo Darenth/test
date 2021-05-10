702143f70d0a04a2459db01e7a6dd344bd6769b3
Darenth <darenth0@gmail.com>
2021-05-10 15:14:29 +0200

    the third  commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Hello from the first commit");
+        System.out.printf("Hello from the 2 commit");
+        System.out.printf("Hello from the 3 commit");
+        System.out.printf("Hello from the 4 commit");
 
     }
 }
