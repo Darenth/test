d017c44daeb595d9ff3e9375d959e5c9916ac347
Darenth <darenth0@gmail.com>
2021-05-10 15:10:46 +0200

    the second  commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Hello from the first commit");
-        System.out.printf("Hello from the second commit");
+
     }
 }
